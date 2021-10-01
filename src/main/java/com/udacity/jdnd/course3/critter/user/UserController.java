90e93d3cb90fc96a6bfbaea6dfd1620e1b177b95
Andrew Burkhart <burkhartfc@icloud.com>
2021-10-01 12:04:04 -0700

    Wrap all routes in try catch for error handling

--- src/main/java/com/udacity/jdnd/course3/critter/user/UserController.java
+++ src/main/java/com/udacity/jdnd/course3/critter/user/UserController.java
@@ -31,18 +31,29 @@ public class UserController {
 
     @PostMapping("/customer")
     public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO) {
-        Customer customer = customerService.fromRequestCustomer(customerDTO);
-        Customer savedCustomer = customerService.save(customer);
-        return customerService.forResponseCustomer(savedCustomer);
+        try {
+            Customer customer = customerService.fromRequestCustomer(customerDTO);
+            Customer savedCustomer = customerService.save(customer);
+            return customerService.forResponseCustomer(savedCustomer);
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "Unable to save Customer. Please check the data and try again."
+            );
+        }
     }
 
     @GetMapping("/customer")
     public List<CustomerDTO> getAllCustomers(){
-        List<Customer> customers = customerService.findAll();
-        return customers
+        try {
+            List<Customer> customers = customerService.findAll();
+            return customers
                 .stream()
                 .map(customer -> customerService.forResponseCustomer(customer))
                 .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Could not find any Customers");
+        }
     }
 
     @GetMapping("/customer/pet/{petId}")
@@ -52,35 +63,63 @@ public class UserController {
         } catch (Exception e) {
             // https://www.baeldung.com/spring-response-status-exception#2-different-status-code---same-exception-type
             throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND,
-                    "Unable to find customer with a pet matching that ID"
+                HttpStatus.NOT_FOUND,
+                "Unable to find Customer with a Pet matching that ID"
             );
         }
     }
 
     @PostMapping("/employee")
     public EmployeeDTO saveEmployee(@RequestBody EmployeeDTO employeeDTO) {
-        Employee employee = employeeService.fromRequestEmployee(employeeDTO);
-        Employee savedEmployee = employeeService.save(employee);
-        return employeeService.forResponseEmployee(savedEmployee);
+        try {
+            Employee employee = employeeService.fromRequestEmployee(employeeDTO);
+            Employee savedEmployee = employeeService.save(employee);
+            return employeeService.forResponseEmployee(savedEmployee);
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "Unable to save Employee. Please check the data and try again."
+            );
+        }
     }
 
     @PostMapping("/employee/{employeeId}")
     public EmployeeDTO getEmployee(@PathVariable long employeeId) {
-        return employeeService.forResponseEmployee(employeeService.findById(employeeId));
+        try {
+            return employeeService.forResponseEmployee(employeeService.findById(employeeId));
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND,
+                "Unable to Employee matching that ID."
+            );
+        }
     }
 
     @PutMapping("/employee/{employeeId}")
     public void setAvailability(@RequestBody Set<DayOfWeek> daysAvailable, @PathVariable long employeeId) {
-        employeeService.setAvailability(daysAvailable, employeeId);
+        try {
+            employeeService.setAvailability(daysAvailable, employeeId);
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "Unable to set availability. Please check the data and try again."
+            );
+        }
     }
 
     @GetMapping("/employee/availability")
     public List<EmployeeDTO> findEmployeesForService(@RequestBody EmployeeRequestDTO employeeDTO) {
-        return employeeService.findBySkill(employeeDTO.getSkills(), employeeDTO.getDate())
+        try {
+            return employeeService.findBySkill(employeeDTO.getSkills(), employeeDTO.getDate())
                 .stream()
                 .map(employee -> employeeService.forResponseEmployee(employee))
                 .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND,
+                "Unable to find Employee matching those parameters."
+            );
+        }
     }
 
 }
