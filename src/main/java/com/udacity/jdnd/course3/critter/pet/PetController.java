dcb0546ae03b84897aad1c70df5a021f8b043735
Andrew Burkhart <burkhartfc@icloud.com>
2021-10-01 15:29:22 -0700

    Update Pet Controller and service to support

--- src/main/java/com/udacity/jdnd/course3/critter/pet/PetController.java
+++ src/main/java/com/udacity/jdnd/course3/critter/pet/PetController.java
@@ -1,8 +1,14 @@
 package com.udacity.jdnd.course3.critter.pet;
 
+import com.udacity.jdnd.course3.critter.entities.Pet;
+import com.udacity.jdnd.course3.critter.services.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Handles web requests related to Pets.
@@ -11,23 +17,61 @@ import java.util.List;
 @RequestMapping("/pet")
 public class PetController {
 
+    @Autowired
+    private PetService petService;
+
     @PostMapping
     public PetDTO savePet(@RequestBody PetDTO petDTO) {
-        throw new UnsupportedOperationException();
+        try {
+            Pet pet = petService.fromRequestPet(petDTO);
+            Pet savedPet = petService.save(pet);
+            return petService.forResponsePet(savedPet);
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "Unable to save Pet. Please check the data and try again."
+            );
+        }
     }
 
     @GetMapping("/{petId}")
     public PetDTO getPet(@PathVariable long petId) {
-        throw new UnsupportedOperationException();
+        try {
+            return petService.forResponsePet(petService.findById(petId));
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND,
+                "Unable to find Pet matching that ID."
+            );
+        }
     }
 
     @GetMapping
     public List<PetDTO> getPets(){
-        throw new UnsupportedOperationException();
+        try {
+            List<Pet> pets = petService.findAll();
+            return pets
+                .stream()
+                .map(pet -> petService.forResponsePet(pet))
+                .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Could not find any Pets");
+        }
     }
 
     @GetMapping("/owner/{ownerId}")
     public List<PetDTO> getPetsByOwner(@PathVariable long ownerId) {
-        throw new UnsupportedOperationException();
+        try {
+            List<Pet> pets = petService.findAllByCustomerId(ownerId);
+            return pets
+                .stream()
+                .map(pet -> petService.forResponsePet(pet))
+                .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND,
+                "Could not find any Pets with an Owner matching that ID"
+            );
+        }
     }
 }
--- src/main/java/com/udacity/jdnd/course3/critter/services/PetService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/services/PetService.java
@@ -1,10 +1,14 @@
 package com.udacity.jdnd.course3.critter.services;
 
+import com.udacity.jdnd.course3.critter.entities.Customer;
 import com.udacity.jdnd.course3.critter.entities.Pet;
+import com.udacity.jdnd.course3.critter.pet.PetDTO;
 import com.udacity.jdnd.course3.critter.repositories.CustomerRepository;
 import com.udacity.jdnd.course3.critter.repositories.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.transaction.Transactional;
 import java.util.List;
@@ -32,5 +36,34 @@ public class PetService {
 
     public List<Pet> findAllByCustomerId(Long id) {
         return customerRepository.getOne(id).getPets();
+    }
+
+    public Pet fromRequestPet(PetDTO petDTO) {
+        Pet pet = new Pet();
+        pet.setName(petDTO.getName());
+        pet.setBirthDate(petDTO.getBirthDate());
+        pet.setType(petDTO.getType());
+        pet.setNotes(petDTO.getNotes());
+        try {
+            Customer customer = customerRepository.getOne(petDTO.getOwnerId());
+            pet.setCustomer(customer);
+            return pet;
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "Unable to save Pet. Please check your customer information and try again."
+            );
+        }
+    }
+
+    public PetDTO forResponsePet(Pet pet) {
+        PetDTO petDTO = new PetDTO();
+        petDTO.setId(pet.getId());
+        petDTO.setName(pet.getName());
+        petDTO.setBirthDate(pet.getBirthDate());
+        petDTO.setType(pet.getType());
+        petDTO.setNotes(pet.getNotes());
+        petDTO.setOwnerId(pet.getCustomer().getId());
+        return petDTO;
     }
 }
