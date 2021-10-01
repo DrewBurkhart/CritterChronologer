763deb2d0a01aa85f865032213e39051c334a465
Andrew Burkhart <burkhartfc@icloud.com>
2021-10-01 15:29:34 -0700

    Update Schedule Controller and service to support

--- src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleController.java
+++ src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleController.java
@@ -1,8 +1,14 @@
 package com.udacity.jdnd.course3.critter.schedule;
 
+import com.udacity.jdnd.course3.critter.entities.Schedule;
+import com.udacity.jdnd.course3.critter.services.ScheduleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Handles web requests related to Schedules.
@@ -11,28 +17,82 @@ import java.util.List;
 @RequestMapping("/schedule")
 public class ScheduleController {
 
+    @Autowired
+    private ScheduleService scheduleService;
+
     @PostMapping
     public ScheduleDTO createSchedule(@RequestBody ScheduleDTO scheduleDTO) {
-        throw new UnsupportedOperationException();
+        // employees, pets, data, activities
+        try {
+            Schedule schedule = scheduleService.fromRequestSchedule(scheduleDTO);
+            Schedule savedSchedule = scheduleService.save(schedule);
+            return scheduleService.forResponseSchedule(savedSchedule);
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "Unable to save Schedule. Please check the data and try again."
+            );
+        }
     }
 
     @GetMapping
     public List<ScheduleDTO> getAllSchedules() {
-        throw new UnsupportedOperationException();
+        try {
+            List<Schedule> schedules = scheduleService.findAll();
+            return schedules
+                .stream()
+                .map(schedule -> scheduleService.forResponseSchedule(schedule))
+                .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Could not find any Schedules");
+        }
     }
 
     @GetMapping("/pet/{petId}")
     public List<ScheduleDTO> getScheduleForPet(@PathVariable long petId) {
-        throw new UnsupportedOperationException();
+        try {
+            List<Schedule> schedules = scheduleService.findScheduleByPetId(petId);
+            return schedules
+                .stream()
+                .map(schedule -> scheduleService.forResponseSchedule(schedule))
+                .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND,
+                "Could not find any Schedules with a Pet matching that ID"
+            );
+        }
     }
 
     @GetMapping("/employee/{employeeId}")
     public List<ScheduleDTO> getScheduleForEmployee(@PathVariable long employeeId) {
-        throw new UnsupportedOperationException();
+        try {
+            List<Schedule> schedules = scheduleService.findScheduleByEmployeeId(employeeId);
+            return schedules
+                .stream()
+                .map(schedule -> scheduleService.forResponseSchedule(schedule))
+                .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND,
+                "Could not find any Schedules with an Employee matching that ID"
+            );
+        }
     }
 
     @GetMapping("/customer/{customerId}")
     public List<ScheduleDTO> getScheduleForCustomer(@PathVariable long customerId) {
-        throw new UnsupportedOperationException();
+        try {
+            List<Schedule> schedules = scheduleService.findScheduleByCustomerId(customerId);
+            return schedules
+                    .stream()
+                    .map(schedule -> scheduleService.forResponseSchedule(schedule))
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "Could not find any Schedules with a Customer matching that ID"
+            );
+        }
     }
 }
